def016f4f7a53f0f12e82862f3b1d01603041bc6
secretcode19 <secretcode19@gmail.com>
2018-12-10 16:23:35 +0900

    20.Using, #include and Namespaces

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+using namespace std;
+
 // the entry point for our application
 int main()
 {
+	cout << "Welcome to Bulls and Cows" << endl;
 	return 0;	// exit the application
 }
