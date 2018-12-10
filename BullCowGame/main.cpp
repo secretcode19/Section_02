a05de130b181996309e17d85628f06caa0324c7e
secretcode19 <secretcode19@gmail.com>
2018-12-10 17:27:34 +0900

    24.Simplifying With Functions

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,16 +3,34 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
 // the entry point for our application
 int main()
 {
+	PrintIntro();
+
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+
+	cout << endl;
+	return 0;	// exit the application
+}
+
+void PrintIntro()
+{
 	// introduce the game
 	constexpr int WORLD_LENGTH = 9;
-
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess " << WORLD_LENGTH;
-	cout << " letter isogram I'm thinking of?\n";
+	cout << " letter isogram I'm thinking of?";
+	cout << endl;
+	return;
+}
 
+string GetGuessAndPrintBack()
+{
 	// get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
@@ -20,13 +38,5 @@ int main()
 
 	// repeat the guess back to them
 	cout << "Your guess was: " << Guess << endl;
-
-	cout << "Enter your guess: ";
-	getline(cin, Guess);
-
-	// repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
-
-	cout << endl;
-	return 0;	// exit the application
+	return Guess;
 }
