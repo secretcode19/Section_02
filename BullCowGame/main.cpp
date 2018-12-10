ff6d7a99a680457defef92798c1622f64e3c3f91
secretcode19 <secretcode19@gmail.com>
2018-12-10 16:36:07 +0900

    21.Magic Numbers and Constants

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -5,6 +5,10 @@ using namespace std;
 // the entry point for our application
 int main()
 {
-	cout << "Welcome to Bulls and Cows" << endl;
+	constexpr int WORLD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Can you guess " << WORLD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+
 	return 0;	// exit the application
 }
