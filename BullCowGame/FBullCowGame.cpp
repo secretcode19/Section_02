bec26aa3022e0d0283e6d5d5a07c2cf82fd3bf51
secretcode19 <secretcode19@gmail.com>
2018-12-11 19:44:17 +0900

    40.Debugging 101

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -48,15 +48,15 @@ FBullCowCount FBullCowGame::SubmitGuess(FString Guess)
 
 	// loop through all letters in the guess
 	int32 HiddenWordLength = MyHiddenWord.length();
-	for (int32 i = 0; i < HiddenWordLength; i++)
+	for (int32 MHWChar = 0; MHWChar < HiddenWordLength; MHWChar++)
 	{
 		// compare letters against the hidden word
-		for (int32 j = 0; j < HiddenWordLength; j++)
+		for (int32 GChar = 0; GChar < HiddenWordLength; GChar++)
 		{
 			// if they match then
-			if (Guess[i] == MyHiddenWord[i])
+			if (Guess[GChar] == MyHiddenWord[MHWChar])
 			{
-				if (i == j)	// if they're in the same place
+				if (MHWChar == GChar)	// if they're in the same place
 				{
 					// increment bulls
 					BullCowCount.Bulls++;
